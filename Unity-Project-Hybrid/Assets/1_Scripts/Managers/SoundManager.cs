43a6bcfad930c2e5dd42beb29a638bcf3be9fee9
Watenk <>
2024-01-12 23:16:11 +0100

    Added Audio

--- Unity-Project-Hybrid/Assets/1_Scripts/Managers/SoundManager.cs
+++ Unity-Project-Hybrid/Assets/1_Scripts/Managers/SoundManager.cs
@@ -4,5 +4,52 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
+    public static SoundManager Instance { get; private set; }
 
+    public AudioSource MusicSource;
+    public AudioSource SfxSource0;
+    public AudioSource SfxSource1;
+
+    public void Awake(){
+        Instance = this;
+    }
+
+    public void PlaySound(AudioClip audioClip, SoundSource source){
+
+        if (source == SoundSource.Music){
+            if (!MusicSource.isPlaying){
+                MusicSource.clip = audioClip;
+                MusicSource.Play();
+            }
+            else{
+                Debug.LogWarning("MusicSource Audio busy");
+            }
+        }
+
+        if (source == SoundSource.SFX0){
+            if (!SfxSource0.isPlaying){
+                SfxSource0.clip = audioClip;
+                SfxSource0.Play();
+            }
+            else{
+                Debug.LogWarning("SfxSource0 Audio busy");
+            }
+        }
+
+        if (source == SoundSource.SFX1){
+            if (!SfxSource1.isPlaying){
+                SfxSource1.clip = audioClip;
+                SfxSource1.Play();
+            }
+            else{
+                Debug.LogWarning("SfxSource1 Audio busy");
+            }
+        }
+    }
+}
+
+public enum SoundSource{
+    Music,
+    SFX0,
+    SFX1
 }
